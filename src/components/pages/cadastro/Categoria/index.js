1d5fc1cc4b46e96005630d3d879a54fd6f0e3127
dyshimizu <danishiro@gmail.com>
2020-07-31 00:23:39 -0300

    pagina de cadastro de categoria

--- src/components/pages/cadastro/Categoria/index.js
+++ src/components/pages/cadastro/Categoria/index.js
@@ -1,11 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageDefault from '../../../PageDefault'
-import { Link } from  'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function CadastroCategoria() {
-    return(
+    const initialCategoryValues = {
+        name: '',
+        description: '',
+        color: '#112233',
+    }
+
+    const [category, setCategory] = useState(initialCategoryValues)
+    const [categorias, setCategorias] = useState([])
+
+    function handleChange(e) {
+        const chave = e.target.getAttribute('name')
+        const value = e.target.value
+
+        setCategory({
+            ...category,
+            [chave]: value
+        })
+
+    }
+
+    return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+            <h1>Cadastro de Categoria - {category.name}</h1>
+
+            <form onSubmit={e => {
+                e.preventDefault()
+                setCategorias([
+                    ...categorias,
+                    category
+                ])
+
+                setCategory(initialCategoryValues)
+            }}>
+                <div>
+                    <label>
+                        Nome da Categoria:
+                    <input
+                            type="text"
+                            value={category.name}
+                            name='name'
+                            onChange={handleChange}
+                        />
+                    </label>
+
+                    <div>
+                        <label>
+                            Descrição:
+                            <textarea
+                                type="text"
+                                value={category.description}
+                                name='description'
+                                onChange={handleChange}
+                            />
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>
+                            Cor:
+                            <input
+                                type="color"
+                                value={category.color}
+                                name='color'
+                                onChange={handleChange}
+                            />
+                        </label>
+                    </div>
+                </div>
+
+                <button>
+                    Cadastrar
+                </button>
+            </form>
+
+            <ul>
+                {categorias.map((categoria, indice) => {
+                    return (
+                        <li style={{ color: `${categoria.color}` }} key={indice}>
+                            {categoria.name}
+                            {' '}
+                            {categoria.description}
+                        </li>
+                    )
+                })
+                }
+            </ul>
 
             <Link to="/">
                 Ir para Home
