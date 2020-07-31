abc452bd5219f3e6dc865d33c562d4f0d165415b
dyshimizu <danishiro@gmail.com>
2020-07-30 22:24:33 -0300

    correção do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
-import { FooterBase }  from './styles'
+import { FooterBase } from './styles'
 
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <FooterBase>
-            <a href="/">
-                <img src=""/>
+            <a href="https://www.alura.com.br/">
+                <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
             </a>
-                Orgulhosamente criado durante a Alura Imersão React
+            <p>
+                Orgulhosamente criado durante a
+          {' '}
+                <a href="https://www.alura.com.br/">
+                    Imersão React da Alura
+          </a>
+            </p>
         </FooterBase>
     )
 }
